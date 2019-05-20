bd8bd1554238f3a6eefdd1f8dc5526d429468a71
avivsab <avivsab19@gmail.com>
2019-05-20 12:25:03 +0300

    Delete state and constuctor from App.js

--- react-map/src/App.js
+++ react-map/src/App.js
@@ -3,19 +3,19 @@ import NavComponent from './components/Nav';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-  }
-}
+//   constructor(props){
+//     super(props);
+//     this.toggle = this.toggle.bind(this);
+//     this.state = {
+//       isOpen: false
+//   }
+// }
   
-toggle() {
-  this.setState({
-    isOpen: !this.state.isOpen
-  });
-}
+// toggle() {
+//   this.setState({
+//     isOpen: !this.state.isOpen
+//   });
+// }
   render() {
     return (
       <div className="App">
