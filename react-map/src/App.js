f3d09a144ccfa5152c4447e0acb5217274a6d99d
avivsab <avivsab19@gmail.com>
2019-05-20 12:10:20 +0300

    Initial files structure with toggled navbar

---
+++ react-map/src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import NavComponent from './components/Nav';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+  }
+}
+  
+toggle() {
+  this.setState({
+    isOpen: !this.state.isOpen
+  });
+}
+  render() {
+    return (
+      <div className="App">
+       <NavComponent/>
+      </div>
+    );
+  }
+}
+  
+
+export default App;
